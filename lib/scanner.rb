1e2b7f757290643119a2cd4266967f8e346e940f
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2016-05-18 22:40:23 -0400

    More better line scanning

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -8,19 +8,37 @@ module Scanner
     return final_array
   end
 
+  def Scanner.shave_token(token, symbol)
+    return_token = token.chomp(symbol)
+    return_token.slice!(0)
+    return return_token
+  end
+
   def Scanner.id_token(token)
     final_array = []
-    if token[0] == "("
+    if token[0] == "(" && token[-1, 1] == ")"
       final_array.push :math_exp
-      final_array.push token
-    elsif token[0] == "\""
+      final_array.push Scanner.shave_token(token, ")")
+    elsif token[0] == "\"" && token[-1, 1] == "\""
       final_array.push :string_lit
-      chomped_token = token.chomp('"')
-      chomped_token.slice!(0)
-      final_array.push chomped_token
+      final_array.push Scanner.shave_token(token, "\"")
+    elsif token[0] == "[" && token[-1, 1] == "]"
+      final_array.push :obj_name
+      final_array.push Scanner.shave_token(token, "]")
+    elsif token == "L"
+      final_array.push :stack_symbol
+    elsif token == ">"
+      final_array.push :branch_symbol
     else
       final_array.push :slot_name
-      final_array.push token
+      if token.include? ?/
+        tmp_array = token.split('/')
+        tmp_array.each do |sub_token|
+          final_array.push sub_token
+        end
+      else
+        final_array.push token
+      end
     end
     return final_array
   end
@@ -37,4 +55,9 @@ module Scanner
     return final_array
   end
 
+  def Scanner.scan_line(line, line_number)
+    tokenized_line = Scanner.tokenize(line, line_number)
+    taged_line = Scanner.tag_tokens(tokenized_line)
+  end
+ 
 end
--- test/test_scanner.rb
+++ test/test_scanner.rb
@@ -9,18 +9,43 @@ class TestScanner < Minitest::Test
     assert_equal [1,"SimpleTest","foo"], Scanner.tokenize( test_line, 1 )
   end
 
+  def test_shave_token
+    test_token = "(with parens)"
+    assert_equal "with parens", Scanner.shave_token( test_token, ")" )
+    test_token = "\"with quotes\""
+    assert_equal "with quotes", Scanner.shave_token( test_token, "\"" )
+    test_token = "[with brackets]"
+    assert_equal "with brackets", Scanner.shave_token( test_token, "]" )
+  end
+
   def test_id_token
     test_token = "SimpleTest"
     assert_equal [:slot_name, "SimpleTest"], Scanner.id_token( test_token )
+    test_token = "ObjectName/SlotName/SlotValue"
+    assert_equal [:slot_name, "ObjectName", "SlotName", "SlotValue"], Scanner.id_token( test_token )
     test_token = "(1 + 1)"
-    assert_equal [:math_exp, "(1 + 1)"], Scanner.id_token( test_token )
+    assert_equal [:math_exp, "1 + 1"], Scanner.id_token( test_token )
     test_token = "\"String Literal\""
     assert_equal [:string_lit, "String Literal"], Scanner.id_token( test_token )
+    test_token = "[ObjectName]"
+    assert_equal [:obj_name, "ObjectName"], Scanner.id_token( test_token )
+    test_token = "L"
+    assert_equal [:stack_symbol], Scanner.id_token( test_token )
+    test_token = ">"
+    assert_equal [:branch_symbol], Scanner.id_token( test_token )
   end
 
   def test_tag_tokens
     tokenized_line = [1,"SimpleTest","foo"]
     assert_equal [1, [:slot_name, "SimpleTest"], [:slot_name, "foo"]], Scanner.tag_tokens( tokenized_line )
+    tokenized_line = [2,"[main]","SomeSlotName","(3)"]
+    assert_equal [2, [:obj_name, "main"], [:slot_name, "SomeSlotName"], [:math_exp, "3"]], Scanner.tag_tokens( tokenized_line )
+  end
+
+  def test_scan_line
+    line =  "L > SlotName SlotNameorValue SlotNameorValue "
+    test_array = [3, [:stack_symbol], [:branch_symbol], [:slot_name, "SlotName"], [:slot_name, "SlotNameorValue"], [:slot_name, "SlotNameorValue"]]
+    assert_equal test_array, Scanner.scan_line( line, 3 )
   end
 
 end
