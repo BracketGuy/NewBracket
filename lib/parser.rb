60b2b4a76d56b1f5f5c56e1dbfd094765585df80
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2016-05-22 22:37:32 -0400

    Finally got that damn test to pass

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,7 +1,18 @@
 module Parser
 
   def Parser.group_slots( source )
-    # Dear god, I have no idea how to do this.
+    object_id = 0
+    output_array = []
+    source.each_with_index do |line,index|
+      if line[0] == :obj_name
+        object_id += 1
+        output_array.push []
+        output_array[object_id - 1].push line[1]
+      else
+        output_array[object_id - 1].push line
+      end
+    end
+    return output_array
   end
 
 end
