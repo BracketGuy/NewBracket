fe47c89f427ffca978311d38c0d8bfac89ba11f9
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2016-05-24 23:39:41 -0400

    Added more functionality to Parser module

--- lib/parser.rb
+++ lib/parser.rb
@@ -15,4 +15,50 @@ module Parser
     return output_array
   end
 
+  def Parser.parse_slot( source )
+    output_hash = {}
+
+    if source.length == 2                        # Only two things in a slot should be us asking another object to be evaluated. (obj name and term)
+      output_hash[:slot_name] = source[0][1]
+      output_hash[:slot_type] = "object_reference"
+      output_hash[:slot_value] = nil
+    elsif source.length == 3                     # Three things in a slot is a slot name, a slot value, 
+      source.each_with_index do |token,index|
+        if token[0] == :slot_name && index == 0
+          output_hash[:slot_name] = token[1]
+        elsif token[0] == :string_lit
+          output_hash[:slot_type] = "string"
+          output_hash[:slot_value] = token[1]
+        end
+      end
+    else                                         # Is more than three tokens in a slot a valid case?
+      #                                          
+    end
+
+    return output_hash
+  end
+
+  def Parser.stuff_slots( source )
+    output_array = []
+    source.each do |object|
+      this_object = []
+      object.each_with_index do |token,index|
+        if index == 0
+          this_object.push token
+        else
+          
+          # MAGIC!!!!!
+
+          # Magic call to parse_slot for each chunk of slot tokens goes here.
+          
+          # MAGIC!!!!!
+
+        end
+      end
+      output_array.push this_object
+      this_object = []
+    end
+    return output_array
+  end
+
 end
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -15,4 +15,19 @@ class TestParser < Minitest::Test
     assert_equal grouped, Parser.group_slots( ungrouped )
   end
 
+  def test_parse_slot 
+    unparsed = [[:slot_name, "ObjectName"], [:term_symbol]]
+    parsed = { slot_name: "ObjectName", slot_type: "object_reference", slot_value: nil }
+    assert_equal parsed, Parser.parse_slot( unparsed )
+    unparsed = [[:slot_name, "string"], [:string_lit, "foo"], [:term_symbol]]
+    parsed = { slot_name: "string", slot_type: "string", slot_value: "foo" }
+    assert_equal parsed, Parser.parse_slot( unparsed )
+  end
+
+  def test_stuff_slots
+    stuffed = [["main", { slot_name: "string", slot_type: "string", slot_value: "foo" }]]
+    unstuffed = [["main", [:slot_name, "string"], [:string_lit, "foo"], [:term_symbol]]]
+    #assert_equal stuffed, Parser.stuff_slots( unstuffed )
+  end
+
 end
